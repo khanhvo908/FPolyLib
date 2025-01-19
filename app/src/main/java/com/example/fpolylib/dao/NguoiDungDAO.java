d613067fcc0a94e8309ce4cbb5eef92fae68d43d
Phong908 <122242560+khanhvo908@users.noreply.github.com>
2025-01-19 13:33:55 +0700

    Update NguoiDungDAO.java

--- app/src/main/java/com/example/fpolylib/dao/NguoiDungDAO.java
+++ app/src/main/java/com/example/fpolylib/dao/NguoiDungDAO.java
@@ -1,6 +1,7 @@
 package com.example.fpolylib.dao;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -8,15 +9,21 @@ import com.example.fpolylib.database.DbHelper;
 
 public class NguoiDungDAO {
     private DbHelper dbHelper;
+    SharedPreferences sharedPreferences;
 
     public NguoiDungDAO(Context context) {
         dbHelper = new DbHelper(context);
+        sharedPreferences = context.getSharedPreferences("dataUser", Context.MODE_PRIVATE);
     }
 
     //kiem tra thon tin dang nhap
     public boolean kiemTraDangNhap(String username, String password) {
         SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
         Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM USER WHERE username = ? AND password = ?", new String[]{username, password});
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+
         return cursor.getCount() > 0;
     }
 
